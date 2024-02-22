92d1f26d59d3fbddfa67ca8fc476fe8623ea35eb
cedri <cedri@CEDRICJAMES>
2024-02-23 07:28:52 +0800

    Add project files.

---
+++ DataTypeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataTypeApp
+{
+    class DataTypeProgram
+    {
+        static void Main(string[] args)
+        {
+            int intValue = 10;
+            double doubleValue = 3.14;
+            char charValue = 'A';
+            bool boolValue = true;
+            string stringValue = "ced";
+
+            Console.WriteLine($"integer: {intValue}");
+            Console.WriteLine($"double: {doubleValue}");
+            Console.WriteLine($"character: {charValue}");
+            Console.WriteLine($"boolean: {boolValue}");
+            Console.WriteLine($"String: {stringValue}");
+
+        }
+    }
+}
